025e2f2e90a99d159ca6277a427e91908ca61aab
Willjay Tseng <willjay.tseng@volt.ai>
2021-12-18 11:01:31 -0800

    update upload s3

--- update_to_s3.py
+++ update_to_s3.py
@@ -1,28 +1,40 @@
 import os
 import boto3
+from datetime import datetime
 import argparse
 
 bucket = 'volt-models'
-key = 'yolov4-tiny/'
+now = datetime.utcnow().strftime('%Y-%m-%d-%H:%M:%S')
+key = f'{now}/'
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--source", required=True,
-                    help="please specify source dir for training result")
+                    help="please specify source dir for training result dir")
 
 args = vars(parser.parse_args())
 
+if not os.path.isdir(args["source"]):
+    print("please specify source dir for training result dir")
+else:
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    )
+    try:
+        for root, dirs, files in os.walk(args["source"]):
+            for file in files:
+                file_path = os.path.join(root, file)
+                s3_key = key+file
+                s3.upload_file(file_path, bucket, s3_key)
+        print(f"== finishing uploading files to {bucket}/{key} ==")
+    except Exception as e:
+        print("FAILED: ", e)
 
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
-)
 
-key += args["source"]
 
-try:
-    s3.upload_file(filename=args["source"], bucket=bucket, key=key)
-    print("== upload file completed ==")
 
-except Exception as e:
-    print("FAILED: ", e)
+
+
+
